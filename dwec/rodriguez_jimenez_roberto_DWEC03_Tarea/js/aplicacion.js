c53c3db6e00da3e3ac627a8b5d71a4f5558776d6
robertorodriguezNet <robertorodriguez@robertorodriguez.net>
2023-11-12 12:56:00 +0100

    DWEC03 primera parte

--- dwec/rodriguez_jimenez_roberto_DWEC03_Tarea/js/aplicacion.js
+++ dwec/rodriguez_jimenez_roberto_DWEC03_Tarea/js/aplicacion.js
@@ -1,3 +1,6 @@
+// Declaración de la ventana
+let app;
+
 /**
  * Constante que recibe la ejecución de una función que inicializa
  * los métodos onlick de los botones.
@@ -7,8 +10,11 @@ const inicializar = () => {
     document.getElementById("cerrar-app").onclick=cerrarApp;
 }
 
-// Declaración de la ventana
-let app;
+// Dimensiones de la ventana
+const WIDTH = 800;
+const HEIGHT = 600;
+
+
 
 /**
  * Se inicia la aplicación abriendo una nueva ventana
@@ -16,8 +22,26 @@ let app;
  */
 const iniciarApp = () => {
 
+    // Cerramos la ventana si ya tenemos una instancia
+    if(app!=null){
+        app.close();
+    }
+
     // open(destino, título, opciones)
-    app = window.open('', 'Aplicación','width=1200,height=600');
+    // window no siempre tiene el control sobre el tamaño de la ventana.
+    // No se indica el parámetro resizable=no para que se ejecute el evento onresize
+    app = window.open('', 'Aplicación',`width=${WIDTH},height=${HEIGHT}`);
+
+    // La ventana no debería redimensionarse por defecto, pero no funciona.
+    // Para ello uso el método onresize para capturar el evento cuando se 
+    // intenta cambiar el tamaño de la ventan.
+    // resizeTo() establece en nuevo valor de la ventana.
+    app.onresize = () => app.resizeTo(WIDTH,HEIGHT);
+
+    // Llamar a la función que creará el contenido
+    crearContenido();
+    
+    
 }
 
 /**
@@ -28,4 +52,25 @@ const cerrarApp = () => {
         app.close();
         app = null;
     }
+}
+
+/**
+ * Función para dar contenido a la ventana abierta
+ */
+function crearContenido(){
+    
+    let documento = app.document;
+    let url = location.href;
+    let protocolo = location.protocol;
+    let navegador = navigator.userAgent;
+    let java = navigator.javaEnabled();
+
+    documento.write("<h3>Ejemplo de nueva ventana</h3>");
+    documento.write("<dl>")
+    documento.write(`<dt>URL:</dt><dd>${url}</dd>`);
+    documento.write(`<dt>Protocolo:</dt><dd>${protocolo}</dd>`);
+    documento.write(`<dt>Navegador:</dt><dd>${navegador}</dd>`);
+    documento.write('<dt>Java habilitado:</dt><dd>Java ' + (java? 'SI' : 'NO') + ' disponible en esta ventana</dd>');
+    documento.write('</dl>')
+
 }
