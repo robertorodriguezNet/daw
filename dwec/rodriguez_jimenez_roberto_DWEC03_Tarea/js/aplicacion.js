30ee7da392eb0f6a60b3c0bbe01755d18a983bc4
robertorodriguezNet <robertorodriguez@robertorodriguez.net>
2023-11-14 12:41:41 +0100

    DWEC03 corregido y finalizado.
    Se ha corregido el ejercicio para que todo el código se ejecute desde
    la ventana secundaria.
    Generado el pdf.

--- dwec/rodriguez_jimenez_roberto_DWEC03_Tarea/js/aplicacion.js
+++ dwec/rodriguez_jimenez_roberto_DWEC03_Tarea/js/aplicacion.js
@@ -1,6 +1,10 @@
 // Declaración de la ventana
 let app;
 
+// Dimensiones de la ventana
+const WIDTH = 900;
+const HEIGHT = 900;
+
 /**
  * Constante que recibe la ejecución de una función que inicializa
  * los métodos onlick de los botones.
@@ -9,27 +13,6 @@ const inicializar = () => {
   document.getElementById("iniciar-app").onclick = iniciarApp;
   document.getElementById("cerrar-app").onclick = cerrarApp;
 };
-
-/**
- * Deveulve los años resultantes de obtener la diferencia
- * entre dos fechas.
- * @param {Date} fecha la fecha de nacimiento
- * @returns la fecha resultante en formato 'aaaa'
- */
-function getEdad(fecha) {
-  // Obtener la edad en milisegundos
-  // Date.now() obtiene el momento actual en milisegundos
-  // edadEnMilisegundos es un objeto Date con el poder trabajar con fechas.
-  let edadEnMilisegundos = new Date(Date.now() - fecha);
-
-  // Devolvemos los años entre la edadEnMilisegundos y 1970.
-  // El 1/1/1970 se toma como en inicio de la cuenta del tiempo (Timestamp)
-  return Math.abs(edadEnMilisegundos.getUTCFullYear() - 1970);
-}
-
-// Dimensiones de la ventana
-const WIDTH = 900;
-const HEIGHT = 900;
 
 /**
  * Se inicia la aplicación abriendo una nueva ventana
@@ -55,11 +38,12 @@ const iniciarApp = () => {
 
   // -- CONTENIDO PARA LA APLICACIÓN ------------------------------------
   // Llamar a la función que creará el contenido
-  crearContenido();
+  app.document.write(crearContenido());
 
   // -- GESTIÓN DE LOS DATOS QUE SE MOSTRARÁN EN LA VENTANA PRINCIPAL ---
+  // A partir de aquí se desarrolla la segunda parte del ejercicio
 
-  // Dias de la semana
+  // Dias de la semana, ordenados sugún el inicio de la semana
   const semana = [
     "jueves",
     "viernes",
@@ -70,63 +54,56 @@ const iniciarApp = () => {
     "miércoles",
   ];
 
-  // Variable que se utiliza como un comodín para parsear los datos
-  // recogidos del prompt
-  let data;
+  // Referenciamos la botonera para poder insertar los elementos correctamente
+  let botonera = document.getElementById("botonera");
 
-  // Variable para html que se mostrará
-  let html;
+  // Establecemos el h1
+  // Crear el elemento que se va a insertar
+  let h1 = document.createElement('h1');
 
-  // Para la fecha de tipo Dae
-  let fecha;
+  // Crear el texto del elemento
+  let h1Text = document.createTextNode('TAREA DWEC03');
 
-  // Variables para los datos solicitados.
-  let nombre_apellidos;
-  let dia;
-  let mes;
-  let anno;
+  // Insertar el texto en el elemento
+  h1.appendChild(h1Text);
 
-  document.write("<h1>TAREA DWEC03</h1>");
+  // Insertar el elemento en el cuerpo de la ventana principal
+  // antes de la botonera
+  document.body.insertBefore(h1,botonera);
+
 
   // Solicitud de datos
-  nombre_apellidos = app.prompt("Escriba su nombre y apellidos");
-  dia = parseInt(app.prompt("Día de nacimiento"));
-  mes = parseInt(app.prompt("Mes de nacimiento (número)"));
-  anno = parseInt(app.prompt("Año de nacimiento"));
-  fecha = new Date(anno, mes, dia);
+  let nombre_apellidos = app.prompt("Escriba su nombre y apellidos");
+  let dia = parseInt(app.prompt("Día de nacimiento"));
+  let mes = parseInt(app.prompt("Mes de nacimiento (número)"));
+  let anno = parseInt(app.prompt("Año de nacimiento"));
+  let fecha = new Date(anno, mes, dia);
 
-  html = `Buenos días ${nombre_apellidos}.
-  <br>
-  Tu nombre tiene ${nombre_apellidos.length} caracteres, incluidos espacios.
-  <br>
-  La primera letra A de tu nombre está en la posición ${
-    nombre_apellidos.indexOf("a") + 1
-  }.
-  <br>
-  La última letra A de tu nombre está en la posición:  ${
-    nombre_apellidos.lastIndexOf("a") + 1
-  }.
-  <br>
-  Tu nombre menos las 3 primeras letras es:  ${nombre_apellidos.substring(3)}.
-  <br>
-  Tu nombre todo en mayúsculas es:   ${nombre_apellidos.toUpperCase()}.
-  <br>
-  Tu edad es: ${isNaN(getEdad(fecha)) ? " -- " : getEdad(fecha)} años.
-  <br>
-  Naciste un feliz ${
-    typeof semana[fecha.getDay()] === "undefined"
-      ? " -- "
-      : semana[fecha.getDay()]
-  } del año ${isNaN(anno) ? " ---- " : anno}.
-  <br>
-  El coseno de 180 es: ${Math.cos(180)}.
-  <br>
-  El número mayor de 6(34,7,23,75,35,19) es: ${Math.max(34, 7, 23, 75, 35, 19)}
-  <br>
-  Ejemplo de número al azar: ${parseInt(Math.random() * 1000)}
-`;
+  // Insertamos una lista como nodo para la ventana principal
+  // y ajustamos los estilos.
+  let lista = document.createElement('ul');
+  lista.style.listStyle = 'none';
+  lista.style.padding = 0;
 
-  document.write(html);
+  // Creamos los elementos de la lista
+  lista.innerHTML = `<li>Buenos días ${nombre_apellidos}.</li>
+  <li>Tu nombre tiene ${nombre_apellidos.length} caracteres, incluidos espacios.</li>
+  <li>La primera letra A de tu nombre está en la posición ${nombre_apellidos.indexOf("a") + 1}</li>
+  <li>Tu nombre menos las 3 primeras letras es:  ${nombre_apellidos.substring(3)}.</li>
+  <li>Tu nombre todo en mayúsculas es:   ${nombre_apellidos.toUpperCase()}.</li>
+  <li>Tu edad es: ${isNaN(getEdad(fecha)) ? " -- " : getEdad(fecha)} años.</li>
+  <li>Naciste un feliz ${
+        typeof semana[fecha.getDay()] === "undefined"
+          ? " -- "
+          : semana[fecha.getDay()]
+      } del año ${isNaN(anno) ? " ---- " : anno}.</li>
+  <li>El coseno de 180 es: ${Math.cos(180)}.</li>
+  <li>El número mayor de 6(34,7,23,75,35,19) es: ${Math.max(34,7,23,75,35,19)}</li>
+  <li>Ejemplo de número al azar: ${parseInt(Math.random() * 1000)}</li>`;
+
+  // Inserta la lista antes de la botonera
+  document.body.insertBefore(lista,botonera);
+
 };
 
 /**
@@ -156,29 +133,33 @@ function crearContenido() {
   // javaEnabled() en un método obsoleto.
   // No se recomienda su uso y la alternativa es consultar
   // la compatibilidad del navegador.
-  let java = navigator.javaEnabled();
+  let java = navigator.javaEnabled() ? "SÍ" : "NO";
 
   // Header con el título.
-  documento.write("<h3>Ejemplo de nueva ventana</h3>");
+  return `<h3>Ejemplo de nueva ventana</h3>
+    <dl>
+      <dt>URL:</dt><dd>${url}</dd>
+      <dt>Protocolo:</dt><dd>${protocolo}</dd>
+      <dt>Navegador:</dt><dd>${navegador}</dd>
+      <dt>Java habilitado:</dt><dd>Java ${java} disponible en esta ventana</dd>  
+    </dl>
 
-  // Lista de definiciones con los datos que se piden.
-  documento.write("<dl>");
-  documento.write(`<dt>URL:</dt><dd>${url}</dd>`);
-  documento.write(`<dt>Protocolo:</dt><dd>${protocolo}</dd>`);
-  documento.write(`<dt>Navegador:</dt><dd>${navegador}</dd>`);
-  documento.write(
-    "<dt>Java habilitado:</dt><dd>Java " +
-      (java ? "SI" : "NO") +
-      " disponible en esta ventana</dd>"
-  );
-  documento.write("</dl>");
+    <iframe id="iframe-goole" title="Google" width="800" height="600" src="www.google.es"></iframe>`;
+}
 
-  // iframe solicitando una url que es rechazada.
-  documento.write(`<iframe
-  id="iframe-goole"
-  title="Google"
-  width="800"
-  height="600"
-  src="www.google.es">
-</iframe>`);
+/**
+ * Deveulve los años resultantes de obtener la diferencia
+ * entre dos fechas.
+ * @param {Date} fecha la fecha de nacimiento
+ * @returns la edad
+ */
+function getEdad(fecha) {
+  // Obtener la edad en milisegundos
+  // Date.now() obtiene el momento actual en milisegundos
+  // edadEnMilisegundos es un objeto Date con el poder trabajar con fechas.
+  let edadEnMilisegundos = new Date(Date.now() - fecha);
+
+  // Devolvemos los años entre la edadEnMilisegundos y 1970.
+  // El 1/1/1970 se toma como en inicio de la cuenta del tiempo (Timestamp)
+  return Math.abs(edadEnMilisegundos.getUTCFullYear() - 1970);
 }
