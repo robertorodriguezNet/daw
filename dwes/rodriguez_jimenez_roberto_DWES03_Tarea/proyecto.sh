6069d30ebf8a6f2c96266dfeb688698c175aa775
robertorodriguezNet <robertorodriguez@robertorodriguez.net>
2023-11-19 11:54:19 +0100

    DWES03 modificado el ejercicio 3.1.3 para permisos

--- dwes/rodriguez_jimenez_roberto_DWES03_Tarea/proyecto.sh
+++ dwes/rodriguez_jimenez_roberto_DWES03_Tarea/proyecto.sh
@@ -18,3 +18,7 @@ echo "<?php
 ?>" > /var/www/proyecto/ejercicio-3-1-3.php
 
 chown roberto:roberto -R proyecto
+
+# Permisos para los directorios después de /var/www
+find /var/www/proyecto -type d -exec chmod 2775 {} \;
+find /var/www/proyecto -type f -exec chmod 0664 {} \;
