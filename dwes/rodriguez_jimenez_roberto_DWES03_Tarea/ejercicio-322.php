fc66d09cf7cee0a8f03de186407e7c1668520ada
robertorodriguezNet <robertorodriguez@robertorodriguez.net>
2023-11-22 14:14:50 +0100

    DWES03 ejercicio 3.2.2 OK

--- dwes/rodriguez_jimenez_roberto_DWES03_Tarea/ejercicio-322.php
+++ dwes/rodriguez_jimenez_roberto_DWES03_Tarea/ejercicio-322.php
@@ -1,171 +1,96 @@
 <?php
 
-// La conexión con la base de datos la tenemos que hacer aunque
-// no haya peticiones, ya que tenemos que mostrar el listado de productos.
-$host = 'localhost';
-$port = 3306;
-$user = 'gestor';
-$pass = 'secreto';
-$db = 'proyecto';
+// Utiliza PDO para repartir entre las tiendas las tres unidades que hay de 
+// PAPYRE62GB en la tienda 1
 
-$error = false;
+// |______________________________|
+// | producto | tienda | unidades |
+// |----------|--------|----------|
+// |    15    |    1   |    2     | 
+// |    15    |    2   |    0     |
+// |    15    |    3   |    1     |
+// |__________|________|__________|
 
-// Conectar con la base de datos
-@$cnxDb = mysqli_connect($host, $user, $pass, $db);
+// Preparamos los datos
+$host = "localhost";
+$db = "proyecto";
+$user = "gestor";
+$pass = "secreto";
 
-// Si hay error se cierra la app
-if ($cnxDb->connect_error) {
-    die('Error en la conexión: ' . $cnxDb->connect_error);
+// DSN: Data Source Name (nombre de origen de datos)
+// Especificar el driver: mysql.
+// Se podría especificar el puerto.
+$dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
+
+// Crear el objeto PDO.
+// Para cerrar la conexión hay que eliminar el objeto.
+$conn = new PDO($dsn, $user, $pass);
+
+// Establecemos que el error se muestre como un warning.
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+
+// Consultas
+// Quitar 1 de la tienda 1
+$consultaTienda1 = "UPDATE stocks SET unidades = 1 WHERE (SELECT id FROM productos WHERE nombre_corto='PAPYRE62GB') AND tienda=1";
+$consultaTienda2 = "INSERT INTO stocks (producto,tienda,unidades) VALUES ((SELECT id FROM productos WHERE nombre_corto='PAPYRE62GB'),2,1)";
+
+// Versión corta:
+$consultaTienda2Corta= "INSERT INTO stock SELECT id, 2, 1 FROM productos WHERE nombre_corto='PAPYRE62GB')";
+
+// mensajes
+$MESSAGES = [
+    "ok" =>"<p>
+        Los datos se han actualizado correctamente.
+    </p>",
+    "ko" =>"<p class='text-danger'>
+        No se han podido actualizar los datos.
+    </p>"
+];
+$message = $MESSAGES['ok'];
+
+// Iniciamos la transacción.
+// beginTransaction() desactiva autocommit.
+$conn->beginTransaction();
+
+// Ejecutamos la primera consulta.
+// Las variables recogen true|false.
+$isTienda1Ok = ($conn->exec($consultaTienda1));
+$isTienda2Ok = ($conn->exec($consultaTienda2));
+
+if($isTienda1Ok && $isTienda2Ok){
+    $conn->commit();
+    $message = $MESSAGES['ok'];
+}else{
+    $conn->rollBack();
+    $message = $MESSAGES['ko'];
 }
 
-// Obtener el listado de productos para mostrar en el select
-$consultaDeProductos = 'SELECT id, nombre FROM productos ORDER BY nombre';
-$resultadoDeProductos = $cnxDb->query($consultaDeProductos);
-$listadoDeProductos = $resultadoDeProductos->fetch_object();
-
-// Si existen peticiones por POST
-if(!empty($_POST)){
-
-    // Se quiere actualizar un producto
-    // Actualizamos antes de mostrar la tabla con los productos
-    if(isset($_POST['actualizar'])){
-
-        $consultaParaActualizar = 'UPDATE stocks SET unidades = ? WHERE producto = ? AND tienda = ?';
-        $stmt = $cnxDb->stmt_init();
-        
-        if(!$stmt->prepare($consultaParaActualizar)){
-          //  echo "Se ha producido un error: " . $cnxDb->error;
-            die("error");
-        }
-        
-        $stmt->bind_param('iii', $_POST['unidades'], $_POST['producto'], $_POST['tienda']);
-
-        if(!$stmt->execute()){
-            echo 'Se ha producido un error'. $cnxDb->error;
-            die();
-        }
-
-        $stmt->close();
-
-    } 
-    
-    // Cuando se pide un producto, o se actualiza, se muestra la tabla con las unidades en cada tienda.
-    if (isset($_POST["producto"])) {
-        // Montamos las consultas
-        $ConsultaDeUnProducto = 'SELECT s.tienda, s.unidades, t.nombre FROM stocks as s, tiendas as t WHERE s.producto = ' . $_POST['producto'] . '  AND t.id = s.tienda';
-        // Obtener un resultado
-        $resultadoDeUnProducto = $cnxDb->query($ConsultaDeUnProducto);
-        // Pasamos el resultado a un objeto
-        $stockDeUnProducto = $resultadoDeUnProducto->fetch_object();    
-    }
-
-}
-
-$cnxDb->close();
 ?>
 
 <!DOCTYPE html>
 <html lang="es">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ejercicio 3.1.5</title>
+    <title>Document</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
-
 <body>
+    
+    <h1 class="text-center">Ejercicio 3.2.2</h1>
+    <h2 class="text-center">Uso de transacciones con PDO</h2>
 
-    <div class="container">
-
-        <div class="row">
-            <div class="col-12 text-center" style="width: 100%">
-                <h1>Ejercicio</h1>
-            </div>
-            <div class="col-12 text-center" style="width: 100%">
-                <p>Crea una página web en la que se muestren las unidades existentes de un determinado producto en cada
-                    una de las tiendas.</p>
-            </div>
-        </div>
-
-        <div class="row mt-3 mb-3">
-            <div class="col">
-                <form method="post">
-                    <fieldset class="p-3 rounded border border-gray">
-                        <legend>Seleccionar un producto</legend>
-
-                        <p class="mt-3 mb-3">
-                            <label for="producto">Producto: </label>
-                            <select name="producto" id="producto" class="rounded">
-
-                                <?php while ($listadoDeProductos != null): ?>
-                                    <option value="<?= $listadoDeProductos->id ?>" 
-                                        <?php if(isset($_POST['producto']) && $_POST['producto'] == $listadoDeProductos->id):?>
-                                            selected
-                                            <?php endif; ?>
-                                            >
-                                        <?= $listadoDeProductos->nombre ?>
-                                    </option>
-                                    <?php
-                                    $listadoDeProductos = $resultadoDeProductos->fetch_object();
-                                endwhile;
-                                ?>
-
-                            </select>
-                        </p>
-
-                        <button class="rounded">Mostrar</button>
-                    </fieldset>
-                </form>
-            </div>
-        </div>
-
-        <?php if (isset($_POST['producto'])): ?>
-
-            <div class="row justify-content-center mt-3 mb-3">
-                <div class="col-6">
-                    <table class="table border border-gray rounded">
-
-                        <thead>
-                            <tr>
-                                <th class="text-center bg-light" scope="col">Tienda</th>
-                                <th class="text-center bg-light" scope="col">Uds.</th>
-                                <th class="text-center bg-light" scope="col">&nbsp;</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php while ($stockDeUnProducto != null): ?>
-                                <tr>
-
-                                    <form action="<?=$_SERVER['PHP_SELF']?>" method="POST">
-                                        <input type="hidden" name="tienda" id="tienda" value="<?= $stockDeUnProducto->tienda ?>">
-                                        <input type="hidden" name="producto" id="producto" value="<?= $_POST['producto']?>">
-                                        <td>
-                                            <?= $stockDeUnProducto->nombre ?>
-                                        </td>
-                                        <td class="text-center">
-                                            <input class="text-end" type="number" name="unidades" id="unidades" value="<?= $stockDeUnProducto->unidades ?>" min="0" size="4" step="1">
-                                        </td>
-                                        <td>
-                                            <input type="submit" id="actualizar" name="actualizar" value="Actualizar">
-                                        </td>
-                                    </form>
-
-                                </tr>
-                                <?php $stockDeUnProducto = $resultadoDeUnProducto->fetch_object(); ?>
-                            <?php endwhile; ?>
-                        </tbody>
-
-                    </table>
-
-                </div>
-            </div>
-
-        <?php endif ?>
-
+    <div class="container mt-3 text-center">
+        <p>
+            <?= $message ?>
+        </p>
     </div>
 
     <script src="js/bootstrap.bundle.min.js"></script>
 </body>
-
 </html>
+
+<?php
+    // Eliminar la conexión.
+    $conn = null;
+?>
