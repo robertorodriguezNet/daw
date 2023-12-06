52ca394a00503f9e5d8f8d3f56f4d0da072a18f2
robertorodriguezNet <robertorodriguez@robertorodriguez.net>
2023-12-06 20:15:13 +0100

    Utils: completado el script install-tomcat-tar.sh

--- utils/install_tomcat-tar.sh
+++ utils/install_tomcat-tar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt install openjdk-11-jdk
+apt install default-jdk
 cd /tmp
 curl -O https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.83/bin/apache-tomcat-9.0.83.tar.gz
 mkdir /opt/tomcat
@@ -12,29 +12,33 @@ chmod -R g+r conf
 chmod g+x conf
 chown -R tomcat webapps/ work/ tem/ logs/
 
+echo "
+[Unit]
+Description=Apache Tomcat Web Application Container
+After=network.target
 
-# [Unit]
-# Description=Apache Tomcat Web Application Container
-# After=network.target
+[Service]
+Type=forking
 
-# [Service]
-# Type=forking
+Environment=JAVA_HOME=/usr/lib/jvm/java-1.11.0-openjdk-amd64
+Environment=CATALINA_PID=/opt/tomcat/temp/tomcat.pid
+Environment=CATALINA_HOME=/opt/tomcat
+Environment=CATALINA_BASE=/opt/tomcat
+Environment='CATALINA_OPTS=-Xms512M -Xmx1024M -server -XX:+UseParallelGC'
+Environment='JAVA_OPTS=-Djava.awt.headless=true -Djava.security.egd=file:/dev/./urandom'
 
-# Environment=JAVA_HOME=/usr/lib/jvm/java-1.11.0-openjdk-amd64
-# Environment=CATALINA_PID=/opt/tomcat/temp/tomcat.pid
-# Environment=CATALINA_HOME=/opt/tomcat
-# Environment=CATALINA_BASE=/opt/tomcat
-# Environment='CATALINA_OPTS=-Xms512M -Xmx1024M -server -XX:+UseParallelGC'
-# Environment='JAVA_OPTS=-Djava.awt.headless=true -Djava.security.egd=file:/dev/./urandom'
+ExecStart=/opt/tomcat/bin/startup.sh
+ExecStop=/opt/tomcat/bin/shutdown.sh
 
-# ExecStart=/opt/tomcat/bin/startup.sh
-# ExecStop=/opt/tomcat/bin/shutdown.sh
+User=tomcat
+Group=tomcat
+UMask=0007
+RestartSec=10
+Restart=always
 
-# User=tomcat
-# Group=tomcat
-# UMask=0007
-# RestartSec=10
-# Restart=always
+[Install]
+WantedBy=multi-user.target" > /etc/
 
-# [Install]
-# WantedBy=multi-user.target
+systemctl daemon-reload
+systemctl start tomcat
+Systemctl status tomcat
