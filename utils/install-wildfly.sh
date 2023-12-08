0926deb134a7f1ce5434af185ad9b50c028c9ddb
robertorodriguezNet <robertorodriguez@robertorodriguez.net>
2023-12-08 11:18:58 +0100

    Utils install WildFly: corregido el script

--- utils/install-wildfly.sh
+++ utils/install-wildfly.sh
@@ -3,7 +3,7 @@
 cd /opt
 
 # Obtener el archivo comprimido
-sudo wget https://github.com/wildfly/wildfly/releases/download/26.1.1.Final/wildfly-preview-26.1.1.Final.tar.gz
+sudo wget https://github.com/wildfly/wildfly/releases/download/26.1.1.Final/wildfly-26.1.1.Final.tar.gz
 
 sudo install default-jdk
 
@@ -12,20 +12,20 @@ sudo groupadd --system wildfly
 sudo useradd -s /bin/login --system -d /opt/wildfly -g wildfly wildfly
 
 # Descomprimir el archivo
-sudo tar xzvf wildfly-26.1.3.Final.tar.gz
+sudo tar xzvf wildfly-26.1.1.Final.tar.gz
 
 # Cambiar el nombre del directorio
-mv wildfly-26.1.3.Final wildfly
+mv wildfly-26.1.1.Final wildfly
 
 # Copia de archivos administrativos
 mkdir /etc/wildfly
 sudo cp wildfly/docs/contrib/scripts/systemd/wildfly.conf /etc/wildfly
 sudo cp wildfly/docs/contrib/scripts/systemd/wildfly.service /etc/systemd/system/
-sudo cp wildfly/docs/contrib/scripts/systemd/launch.sh /etc/wildfly/bin
+sudo cp wildfly/docs/contrib/scripts/systemd/launch.sh /opt/wildfly/bin
 
 # Cambiar los permisos
 sudo chmod +x wildfly/bin/launch.sh
-sudo chmod -R wildfly:wildfly wildfly/
+sudo chown -R wildfly:wildfly wildfly/
 sudo chmod -R +x wildfly/
 
 sudo systemctl daemon-reload
