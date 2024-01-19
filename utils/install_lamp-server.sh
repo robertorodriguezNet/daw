d9938c586b6a5ce00ba9805ad7ccf540253aaf36
robertorodriguezNet <robertorodrigue@robertorodriguez.net>
2024-01-19 16:04:14 +0100

    Instalación de lamp mediante comandos tasksel

--- utils/install_lamp-server.sh
+++ utils/install_lamp-server.sh
@@ -6,7 +6,7 @@
 
 
 # Pasos:
-# 1.- Instalar taskel
+# 1.- Instalar tasksel
 # 2.- Instalar lamp-server
 # 3.- Habilitar el módulo userdir de Apache para poder trabajar en nuestro directorio y no tener problemas de permisos.
 # 4.- Crear la carpeta public_html
@@ -15,5 +15,26 @@
 
 clear
 
-# Instalación del taskel
-apt install taskel
+# Instalación del tasksel
+sudo apt install tasksel -y
+
+# Instalación de lamp-server mediante taksel.
+# Se abre modo gráfico
+sudo tasksel install lamp-server
+
+# Habilitar el módulo userdir de Apache.
+# Cambiamos el directorio por defecto de Apache (/var/www) por el directorio del usuario y así evitar problemas con los permisos.
+sudo a2enmod userdir
+mkdir ~/public_html
+sudo systemctl restart apache
+
+# Editar el archivo php*.conf para permitir la ejecución de PHP en html_public
+# En esta instalación se ha instalado php7.4.
+# Hay que comentar el bloque <IfModule mod_userdir.c>
+sudo vim /etc/apache2/mods-enabled/php7.4.cofn
+
+
+# Crear el archivo info.php en pubic.html
+echo "<?php phpinfo(); ?>" >> ~/public_html/info.php
+
+# Reiniciar apache
