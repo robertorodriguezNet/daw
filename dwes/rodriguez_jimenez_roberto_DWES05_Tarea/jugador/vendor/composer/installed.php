495565b591973a4e55d105cd8d5454a981e0d6a3
robertorodriguezNet <robertorodriguez@robertorodriuez.net>
2024-02-14 12:59:00 +0100

    DWES05 Tarea inicializado composer sin dependencias

---
+++ dwes/rodriguez_jimenez_roberto_DWES05_Tarea/jugador/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'roberto/jugador',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '980279231bfdfd1e36c580b61dc5b68e44206c5c',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'roberto/jugador' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '980279231bfdfd1e36c580b61dc5b68e44206c5c',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
