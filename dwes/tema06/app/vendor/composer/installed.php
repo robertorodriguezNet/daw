0c8466e8c3a3b3d17fd11b88eb7d2e5f05848b16
robertorodriguezNet <robertorodriguez@robertorodriguez.net>
2024-03-06 12:30:57 +0100

    DWES06 Inicio del ejercicio del tema

---
+++ dwes/tema06/app/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'roberto/app',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e3682b8e8351de85c818fcf2de6c39b623afb3dc',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'roberto/app' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e3682b8e8351de85c818fcf2de6c39b623afb3dc',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
